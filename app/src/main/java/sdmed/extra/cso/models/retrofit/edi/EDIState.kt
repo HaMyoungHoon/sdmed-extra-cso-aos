5b51245353f05ea594c9cff6c1718222b2af0c3c
HaMyoungHoon <basicouts@gmail.com>
2025-02-07 18:03:51 +0900

    edi model 변경

--- app/src/main/java/sdmed/extra/cso/models/retrofit/edi/EDIState.kt
+++ app/src/main/java/sdmed/extra/cso/models/retrofit/edi/EDIState.kt
@@ -1,9 +1,21 @@
 package sdmed.extra.cso.models.retrofit.edi
 
-enum class EDIState(var index: Int) {
-    None(0),
-    OK(1),
-    Reject(2),
-    Pending(3),
-    Partial(4),
+import sdmed.extra.cso.R
+
+enum class EDIState(var index: Int, var desc: String) {
+    None(0, "미지정"),
+    OK(1, "완료"),
+    Reject(2, "거부"),
+    Pending(3, "보류"),
+    Partial(4, "부분");
+    companion object {
+        fun parseEDIColor(ediState: EDIState?) = when (ediState) {
+            None -> R.color.edi_state_none
+            OK -> R.color.edi_state_ok
+            Reject -> R.color.edi_state_reject
+            Pending -> R.color.edi_state_pending
+            Partial -> R.color.edi_state_partial
+            null -> R.color.edi_state_none
+        }
+    }
 }
--- app/src/main/java/sdmed/extra/cso/models/retrofit/edi/EDIUploadModel.kt
+++ app/src/main/java/sdmed/extra/cso/models/retrofit/edi/EDIUploadModel.kt
@@ -1,6 +1,7 @@
 package sdmed.extra.cso.models.retrofit.edi
 
-import java.util.Date
+import sdmed.extra.cso.bases.FDataModelClass
+import sdmed.extra.cso.fDate.FDateTime
 
 data class EDIUploadModel(
     var thisPK: String = "",
@@ -12,10 +13,17 @@ data class EDIUploadModel(
     var orgName: String = "",
     var name: String = "",
     var ediState: EDIState = EDIState.None,
-    var regDate: Date = Date(),
+    var regDate: String = "",
     var etc: String = "",
     var pharmaList: MutableList<EDIUploadPharmaModel> = arrayListOf(),
     var fileList: MutableList<EDIUploadFileModel> = arrayListOf(),
     var responseList: MutableList<EDIUploadResponseModel> = arrayListOf()
-) {
+): FDataModelClass<EDIUploadModel.ClickEvent>() {
+
+    fun getYearMonth() = "${year}-$month"
+    fun getRegDateString() = FDateTime().setThis(regDate).toString("yyyy-MM")
+    fun getEdiColor() = EDIState.parseEDIColor(ediState)
+    enum class ClickEvent(var index: Int) {
+        OPEN(0)
+    }
 }
